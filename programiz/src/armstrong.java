3a651c1c8009c05ece2bb79013219a69d800e586
Suruchi Sagar <suruchisagar24@gmail.com>
2023-04-16 19:52:30 +0530

    second commit

--- programiz/src/armstrong.java
+++ programiz/src/armstrong.java
@@ -3,14 +3,27 @@ import java.util.Scanner;
 public class armstrong {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
-        System.out.println("Enter a number here:");
+        System.out.print("Enter a number here:");
         int n= in.nextInt();
         int original_no=n;
+        int a=n;
         int sum=0;
+        int count=0;
+        //counting digits
         while(n!=0){
-            int rem= n%10;
-            sum+=(rem*rem*rem);
             n/=10;
+            count++;
+        }
+        while(a!=0){
+            //finding the digits
+            int rem=a%10;
+            int prod=1;
+            //calculating a^b using for loop
+            for(int i=1;i<=count;i++) {
+                prod = prod * rem;
+            }
+                sum=sum+prod;
+                a/=10;
         }
         if(original_no==sum){
             System.out.println("Armstrong no.");
--- programiz/src/armstrongbtinterval.java
+++ programiz/src/armstrongbtinterval.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class armstrongbtinterval {
+
+//armstrong no. between intervals
+import java.util.Scanner;
+public class armstrongbtinterval {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Enter first term:");
+        int a= in.nextInt();
+        System.out.print("Enter last term:");
+        int b= in.nextInt();
+        for(int i=a; i<=b; i++){
+            int original_no=i;
+            int sum=0;
+
+            while(i!=0){
+                int rem=i%10;
+                sum+=(rem*rem*rem);
+                i/=10;
+            }
+            if(original_no==sum){
+                System.out.println(original_no);
+            }
+
+        }
+    }
 }
